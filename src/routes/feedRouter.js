b966e6109ad078bec64bcbcbf9433a5a9e2ac333
DoorWinBell <msj102525@gmail.com>
2023-05-27 19:01:06 +0900

    getLieksFeedsBySelectedUserId

--- src/routes/feedRouter.js
+++ src/routes/feedRouter.js
@@ -6,12 +6,12 @@ const checkLogInToken = require('../utils/auth');
 const feedController = require('../controllers/feedController');
 const commentController = require('../controllers/commentController');
 const bestController = require('../controllers/bestController');
-const auth = require('../utils/auth')
 
 router.get('', feedController.getAllFeed);
 router.get('/best', feedController.getAllFeed);
 router.get('/:feedId', feedController.getAllFeed);
 router.get('/users/:targetUserId', feedController.getAllFeed);
+router.get('/likes/:selectedUserId', feedController.getAllFeed);
 router.get('/followings', auth, feedController.getAllFeedFollowings);
 router.get('/:feedId/comment', commentController.getCommentByFeedId);
 router.post('/upload', checkLogInToken, feedController.uploadFeed);

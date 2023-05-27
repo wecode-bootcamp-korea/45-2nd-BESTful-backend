5aa08e4723dcef9b4c63ad0b9641a53231583305
DoorWinBell <msj102525@gmail.com>
2023-05-27 19:01:06 +0900

    getFeedByFeedId

--- src/services/feedService.js
+++ src/services/feedService.js
@@ -1,7 +1,7 @@
 const feedDao = require('../models/feedDao');
 
-const getAllFeed = async (offset, limit, genderId, seasonId, styleId, orderBy, userId) => {
-  return await feedDao.getAllFeed(offset, limit, genderId, seasonId, styleId, orderBy, userId);
+const getAllFeed = async (feedId, offset, limit, genderId, seasonId, styleId, orderBy, userId) => {
+  return await feedDao.getAllFeed(feedId, offset, limit, genderId, seasonId, styleId, orderBy, userId);
 };
 
 const uploadFeed = async (userId, description) => {
@@ -10,5 +10,5 @@ const uploadFeed = async (userId, description) => {
 
 module.exports = {
   getAllFeed,
-  uploadFeed
+  uploadFeed,
 };

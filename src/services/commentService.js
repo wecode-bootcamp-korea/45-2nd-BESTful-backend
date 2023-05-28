1338758672449f9b159bc591e9c9f277672bf4b1
DoorWinBell <msj102525@gmail.com>
2023-05-28 16:12:01 +0900

    addComments function

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -4,6 +4,11 @@ const getCommentByFeedId = async (feedId) => {
   return await commentDao.getCommentByFeedId(feedId);
 };
 
+const addComment = async (userId, feedId, contents) => {
+  return await commentDao.addComment(userId, feedId, contents);
+};
+
 module.exports = {
   getCommentByFeedId,
+  addComment,
 };
